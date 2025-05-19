3faf2b0631f9e8ad5ff51492cdb372c3919d997c
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 16:51:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = ">= 1.1.0"
 
   backend "s3" {
-    bucket  = "balastatefilebucket"
-    key     = "tf/notify"
+    bucket  = "mycloudtrailbucket123"
+    key     = "cloudtrailsns/terraform.tfstatec22"
     region  = "us-east-1"
     encrypt = true
-
-    # For Terraform versions below 1.1.0, you need to enable state locking by specifying use_lockfile = true
-    # If you're using Terraform 1.1.0 or later, this is not needed as it's enabled by default
     # use_lockfile = true
   }
 }
+
+
+
